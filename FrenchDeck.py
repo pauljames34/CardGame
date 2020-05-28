5eb6f7ead24061da2742a40ae49c73b1862e26ad
Paul James <mail@paulrichardjames.net>
2020-05-28 15:42:01 +0100

    Create FrenchDeck.py

---
+++ FrenchDeck.py
@@ -0,0 +1,40 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+        
+    def __init__(self):
+        
+        self._cards = [Card(rank, suit)
+                           for suit in self.suits
+                           for rank in self.ranks]
+            
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+    
+    
+        
+                           
+         
+deck = FrenchDeck()
+
+print (len(deck))
+
+print (deck[5])
+
+print (choice('deck'))
+
+print (deck[:3])
+
+print (deck[12::13])
+
+for card in deck:
+    print(card)
